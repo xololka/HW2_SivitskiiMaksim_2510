c31aa35b4fa8ac18193b94da62e4f52c72bf17d1
Maksim Sivitskii <mvsivitskiy@edu.hse.ru>
2026-06-03 17:00:17 +0000

    Добавил остальные тесты

--- test_recipes.py
+++ test_recipes.py
@@ -69,3 +69,82 @@ def test_recipe_len():
     ing2 = Ingredient("Сахар", 200.0, "г")
     rec = Recipe("Пицца", [ing1, ing2])
     assert len(rec) == 2
+
+# Тестирование класса ShoppingList
+
+def test_shopping_list_add_recipe():
+    ing = Ingredient("Мука", 500.0, "г")
+    rec = Recipe("Пицца", [ing])
+    sl = ShoppingList()
+    
+    sl.add_recipe(rec, 2.0)
+    assert len(sl._items) == 1
+    assert sl._items[0][0].quantity == 1000.0
+
+def test_shopping_list_add_recipe_value_error():
+    ing = Ingredient("Мука", 500.0, "г")
+    rec = Recipe("Пицца", [ing])
+    sl = ShoppingList()
+    
+    with pytest.raises(ValueError):
+        sl.add_recipe(rec, -1)
+        
+    with pytest.raises(ValueError):
+        sl.add_recipe(rec, 0)
+
+def test_shopping_list_remove_recipe():
+    ing = Ingredient("Мука", 500.0, "г")
+    rec = Recipe("Пицца", [ing])
+    sl = ShoppingList()
+    
+    sl.add_recipe(rec, 1.0)
+    sl.remove_recipe("Пицца")
+    assert len(sl._items) == 0
+
+def test_shopping_list_remove_recipe_not_found():
+    ing = Ingredient("Мука", 500.0, "г")
+    rec = Recipe("Пицца", [ing])
+    sl = ShoppingList()
+    
+    sl.add_recipe(rec, 1.0)
+    sl.remove_recipe("Суп")
+    assert len(sl._items) == 1
+
+def test_shopping_list_get_list():
+    ing1 = Ingredient("Мука", 500.0, "г")
+    ing2 = Ingredient("Мука", 300.0, "г")
+    ing3 = Ingredient("Апельсин", 2.0, "шт")
+    
+    rec1 = Recipe("Пицца", [ing1])
+    rec2 = Recipe("Пирог", [ing2, ing3])
+    
+    sl = ShoppingList()
+    sl.add_recipe(rec1, 1.0)
+    sl.add_recipe(rec2, 1.0)
+    
+    result = sl.get_list()
+    
+    assert len(result) == 2
+    assert result[0].name == "Апельсин"
+    assert result[0].quantity == 2.0
+    assert result[1].name == "Мука"
+    assert result[1].quantity == 800.0
+
+def test_shopping_list_add():
+    ing1 = Ingredient("Мука", 500.0, "г")
+    ing2 = Ingredient("Сахар", 200.0, "г")
+    
+    rec1 = Recipe("Пицца", [ing1])
+    rec2 = Recipe("Торт", [ing2])
+    
+    sl1 = ShoppingList()
+    sl1.add_recipe(rec1, 1.0)
+    
+    sl2 = ShoppingList()
+    sl2.add_recipe(rec2, 1.0)
+    
+    combined = sl1 + sl2
+    
+    assert len(combined._items) == 2
+    assert len(sl1._items) == 1
+    assert len(sl2._items) == 1
